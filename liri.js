39bc5d1ef2eec999bdec641cc23cf0ad600fda27
yusrob43 <yusuf.robles43@gmail.com>
2017-04-06 21:13:49 -0400

    used correct form of twitter module

--- liri.js
+++ liri.js
@@ -1,13 +1,13 @@
 require("./keys.js");
 
-var twitterAPI = require("node-twitter-api");
+var twitterAPI = require("twitter");
 var spotify = require("spotify");
 var request = require("request");
 
 var app = process.argv[2];
 
 if (app === "my-tweets"){
-
+    
 };
 if (app === "spotify-this-songs"){
 
